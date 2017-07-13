9d0b84e8afdefd5dd7b828fd87aa5839d2d88c45
NAPACN <liuyujie1111@163.com>
2017-07-13 15:37:59 +0800

    1112

--- src/te/de/T.java
+++ src/te/de/T.java
@@ -3,5 +3,8 @@ package te.de;
 public class T {
 	public static void main(String[] args) {
 		System.out.println(11);
+		System.out.println(11);
+		System.out.println(11);
+		System.out.println(11);
 	}
 }
